08879dd21a47857811340891116702533c2a3722
Aleksi1212 <aleksinoro@gmail.com>
2023-08-21 15:35:24 +0300

    get symptoms on getDiagnosisTest route from query params

--- src/app/api/getDiagnosisTest/route.ts
+++ src/app/api/getDiagnosisTest/route.ts
@@ -4,6 +4,11 @@ import type { NextRequest } from 'next/server';
 import getDiagnosis from '@/medicalQueries/diagnosis/getDiagnosis';
 
 export async function GET(request: NextRequest) {
-    const diagnosis = await getDiagnosis(['vomiting', 'itching']);
+    const { searchParams } = new URL(request.url);
+    const symptoms = searchParams.get('symptoms');
+    let decoded: any[] = [];
+    if (symptoms) decoded = JSON.parse(decodeURIComponent(symptoms));
+
+    const diagnosis = await getDiagnosis(decoded);
     return NextResponse.json(diagnosis);
 }
