ac7d29bbd1284b454c65d5a75459ebecb7b01af5
Aleksi1212 <aleksinoro@gmail.com>
2023-08-22 11:07:11 +0300

    logic to create diagnosis

--- src/app/api/createDiagnosis/route.ts
+++ src/app/api/createDiagnosis/route.ts
@@ -1,17 +1,47 @@
 import { NextResponse } from 'next/server';
-import type { NextRequest } from 'next/server';
+import { NextRequest } from 'next/server';
 
-import createDiangosis from '@/medicalQueries/diagnosis/createDiagnosis';
+import createDiagnosis from '@/medicalQueries/diagnosis/createDiagnosis';
+import type { DiagnosisSeverity } from '@/lib/prisma/prismaInit';
 
-export async function GET(request: NextRequest) {
-    // const testDiagnosis1 = await createDiangosis('Fungal infection', 'MEDIUM', ['itching', 'skin rash', 'nodal skin eruptions', 'dischromic patches'])
-    // const testDiagnosis2 = await createDiangosis('Drug reaction', 'MEDIUM', ['itching', 'skin rash', 'stomach pain', 'buring micturition', 'spotting urination'])
-    const test = await createDiangosis('Gastroenteritis', 'MEDIUM', [
-        'vomiting',
-        'sunken eyes',
-        'dehydration',
-        'diarrhoea',
-    ]);
+interface DiagnosisData {
+    name: string;
+    severity: DiagnosisSeverity;
+    symptoms: string[];
+}
 
-    return NextResponse.json(test);
+export async function POST(request: NextRequest) {
+    const body = await request.json();
+    const diagnosisData: DiagnosisData[] = body.diagnosisData || [
+        {
+            name: '',
+            severity: 'LOW',
+            symptoms: [],
+        },
+    ];
+    let createdDiagnosis: any[] = [];
+
+    if (!diagnosisData[0].name) {
+        return NextResponse.json('No data received');
+    }
+
+    for (const data in diagnosisData) {
+        const { name, severity, symptoms } = diagnosisData[data];
+        const { error, errorMessage, diagnosis } = await createDiagnosis(
+            name,
+            severity,
+            symptoms
+        );
+
+        if (error) {
+            console.log(
+                '\x1b[31m%s\x1b[0m',
+                `An error ocurred: ${errorMessage}`
+            );
+            continue;
+        }
+        createdDiagnosis.push(diagnosis);
+    }
+
+    return NextResponse.json(createdDiagnosis);
 }
