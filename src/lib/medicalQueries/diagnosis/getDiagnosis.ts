ba1cb75a5f1571eb5c4585a4c90ee4dea76e6310
Aleksi1212 <aleksinoro@gmail.com>
2023-08-23 15:59:17 +0300

    format

--- src/lib/medicalQueries/diagnosis/getDiagnosis.ts
+++ src/lib/medicalQueries/diagnosis/getDiagnosis.ts
@@ -1,18 +1,12 @@
 import { prisma, type DiagnosisReturnTypes } from '@/lib/prisma/prismaInit';
 import {
     findDiagnosisQuery,
-    findSymptomsFromDiagnosisQuery,
 } from '@/lib/prisma/prismaQueryObjects';
 
 async function getDiagnosis(symptoms: string[]): Promise<DiagnosisReturnTypes> {
     try {
         const diagnosisQueryObject = findDiagnosisQuery(symptoms);
         const diagnosis = await prisma.diagnosis.findMany(diagnosisQueryObject);
-        const diagnosisIds = diagnosis.map((obj) => obj.id);
-
-        // const symptomsQueryObject =
-        //     findSymptomsFromDiagnosisQuery(diagnosisIds);
-        // const everySymptom = await prisma.symptom.findMany(symptomsQueryObject);
 
         return {
             error: false,
