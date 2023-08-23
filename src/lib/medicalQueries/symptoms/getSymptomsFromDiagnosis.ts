e0c8883e26cca08b0d28f83a39b33c81f56a8882
Aleksi1212 <aleksinoro@gmail.com>
2023-08-23 15:58:31 +0300

    get all symptoms from diagnosis

--- src/lib/medicalQueries/symptoms/getSymptomsFromDiagnosis.ts
+++ src/lib/medicalQueries/symptoms/getSymptomsFromDiagnosis.ts
@@ -1,36 +1,34 @@
-// import { prisma, type DiagnosisReturnTypes } from '@/lib/prisma/prismaInit';
-// import {
-//     findDiagnosisQuery,
-//     findSymptomsFromDiagnosisQuery,
-// } from '@/lib/prisma/prismaQueryObjects';
+import { prisma, type SymptomReturnTypes } from '@/lib/prisma/prismaInit';
+import {
+    findDiagnosisQuery,
+    findSymptomsFromDiagnosisQuery,
+} from '@/lib/prisma/prismaQueryObjects';
 
-// async function getSymptomsFromDiagnosis(
-//     symptoms: string[]
-// ): Promise<DiagnosisReturnTypes> {
-//     try {
-//         const diagnosisQueryObject = findDiagnosisQuery(symptoms);
-//         const diagnosis = await prisma.diagnosis.findMany(diagnosisQueryObject);
-//         const diagnosisIds = diagnosis.map((obj) => obj.id);
+async function getSymptomsFromDiagnosis(
+    symptoms: string[]
+): Promise<SymptomReturnTypes> {
+    try {
+        const diagnosisQueryObject = findDiagnosisQuery(symptoms);
+        const diagnosis = await prisma.diagnosis.findMany(diagnosisQueryObject);
+        const diagnosisIds = diagnosis.map((obj) => obj.id);
 
-//         const symptomsQueryObject =
-//             findSymptomsFromDiagnosisQuery(diagnosisIds);
-//         const everySymptom = await prisma.symptom.findMany(symptomsQueryObject);
+        const symptomsQueryObject =
+            findSymptomsFromDiagnosisQuery(diagnosisIds);
+        const possibleDiagnosis = await prisma.symptom.findMany(symptomsQueryObject);
 
-//         return {
-//             error: false,
-//             errorMessage: '',
-//             diagnosis,
-//             everySymptom,
-//         };
-//     } catch (error: any) {
-//         console.error(error);
-//         return {
-//             error: true,
-//             errorMessage: error.message,
-//             diagnosis: [],
-//             everySymptom: [],
-//         };
-//     }
-// }
+        return {
+            error: false,
+            errorMessage: '',
+            possibleDiagnosis
+        };
+    } catch (error: any) {
+        console.error(error);
+        return {
+            error: true,
+            errorMessage: error.message,
+            possibleDiagnosis: [],
+        };
+    }
+}
 
-// export default getSymptomsFromDiagnosis;
+export default getSymptomsFromDiagnosis;
