66d77d9091afdd0efa02a6702cb3488cf44aa36b
Aleksi1212 <aleksinoro@gmail.com>
2023-08-30 12:30:33 +0300

    edge test with postgres query

--- src/app/api/edgeTest/route.ts
+++ src/app/api/edgeTest/route.ts
@@ -1,7 +1,14 @@
 import { type NextRequest, NextResponse } from 'next/server';
+import { prisma } from '@/lib/prisma/prismaInit';
 
 export const runtime = 'edge'
 
 export async function GET(request: NextRequest) {
-    return NextResponse.json('hello keycdn from edge', { status: 200 });
+    const test = await prisma.symptom.findFirst({
+        where: {
+            name: 'päänsärky'
+        }
+    })
+
+    return NextResponse.json(test, { status: 200 });
 }
