75f7c8e69bbaca9dbe55a6a4ae5e2610c7e41fcc
Aleksi1212 <aleksinoro@gmail.com>
2023-08-30 12:10:22 +0300

    keycdn test with edge runtime

--- src/app/api/keycdnTest/route.ts
+++ src/app/api/keycdnTest/route.ts
@@ -1,5 +1,7 @@
 import { type NextRequest, NextResponse } from 'next/server';
 
+export const runtime = 'edge'
+
 export async function GET(request: NextRequest) {
     return NextResponse.json('hello keycdn', { status: 200 });
 }
