b4a00d2a7e21676b1ce9fb4c7356d1f16aa3bb23
Aleksi1212 <aleksinoro@gmail.com>
2023-08-30 12:13:29 +0300

    edge test, and not edge test

--- src/app/api/serverlessTest/route.ts
+++ src/app/api/serverlessTest/route.ts
@@ -1,7 +1,6 @@
 import { type NextRequest, NextResponse } from 'next/server';
 
-export const runtime = 'edge'
 
 export async function GET(request: NextRequest) {
-    return NextResponse.json('hello keycdn', { status: 200 });
+    return NextResponse.json('hello keycdn from serverless', { status: 200 });
 }
